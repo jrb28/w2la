1bdd3240e2e76f3555566d392022303bd26e30d8
jrb28 <james.bradley@mason.wm.edu>
2023-04-01 10:02:32 -0400

    Add files via upload

--- w2la.sql
+++ w2la.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `links`
 --
 
+DROP DATABASE IF EXISTS w2la;
+CREATE DATABASE w2la;
+USE w2la;
+
 DROP TABLE IF EXISTS `links`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
